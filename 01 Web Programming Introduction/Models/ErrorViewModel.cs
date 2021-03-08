b59f2935ebc982806aad1c60d1f0e3b07e4a8e77
bulhard <pravoslav.milenkov@gmail.com>
2021-03-08 20:50:05 +0200

    01 Web Programming Intro project

---
+++ 01 Web Programming Introduction/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _01_Web_Programming_Introduction.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
