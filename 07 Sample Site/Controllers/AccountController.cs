d9de2fc75e71915069a3a3c14fec78fc0ac658de
bulhard <pravoslav.milenkov@gmail.com>
2021-04-19 16:19:04 +0300

    07 - Project & Presentation

---
+++ 07 Sample Site/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using MyBlog.Models.Account;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (loginViewModel.Username == "pravoslav.milenkov@gmail.com") // User Database in real apps
+                {
+                    var user = new { FirstName = "John", LastName = "Doe" };
+
+                    var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                    identity.AddClaim(new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"));
+                    identity.AddClaim(new Claim(ClaimTypes.GivenName, user.FirstName));
+                    identity.AddClaim(new Claim(ClaimTypes.Surname, user.LastName));
+
+                    var principal = new ClaimsPrincipal(identity);
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    loginViewModel.Message = "Incorrect username or password";
+                }
+            }
+            else
+            {
+                loginViewModel.Message = "Invalid input data";
+            }
+
+            return View(loginViewModel);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction(nameof(Login));
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
