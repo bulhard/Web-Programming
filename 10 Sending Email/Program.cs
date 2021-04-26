3319f0a10a0729c4e1b3152b0ad53139319b50ff
bulhard <pravoslav.milenkov@gmail.com>
2021-04-26 11:08:15 +0300

    Presentations 8,9,10

---
+++ 10 Sending Email/Program.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace _10_Sending_Email
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*
+                https://ethereal.email/
+
+                Host	    smtp.ethereal.email
+                Port	    587
+                Security	STARTTLS
+                Name	    Alvah Baumbach
+                Username	alvah.baumbach43@ethereal.email
+                Password	hjhZn93YTDb1Kjd1VA
+             */
+
+            // create email message
+            var email = new MimeMessage();
+
+            email.From.Add(MailboxAddress.Parse("alvah.baumbach43@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("pravoslav.milenkov@gmail.com"));
+
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Plain) { Text = "Example Plain Text Message Body" };
+
+            // send email
+            using var smtp = new SmtpClient();
+
+            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("alvah.baumbach43@ethereal.email", "hjhZn93YTDb1Kjd1VA");
+
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
